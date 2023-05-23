06422bcae2956e8858ccc3d91b97db277da3574c
Satyam Katiyar <satyamkatiyar9993@gmail.com>
2023-05-23 21:27:15 +0530

    adding tab

--- src/Pages/Book.js
+++ src/Pages/Book.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
+import BookTab from '../components/BookTab';
 
 const Book = () => {
+  const [tab, setTab] = useState("inv");
+
   return (
     <div>
       <Header />
       <Navbar />
+      <BookTab setTab={setTab} tab={tab}/>
+      {tab === "inv" && <>inv</>}
+      {tab === "items" && <>items</>}
+      {tab === "exp" && <>exp</>}
     </div>
   )
 }
