94dac2804f11645a98baa0d72a31a12b0ed31448
Satyam Katiyar <satyamkatiyar9993@gmail.com>
2023-05-26 12:14:49 +0530

    yes 3

--- src/components/AddInventory.js
+++ src/components/AddInventory.js
@@ -108,10 +108,10 @@ const AddEmployeeCard = () => {
         <>
             <div className="lg:flex border border-gray-300 cursor-pointer rounded-md">
                 <button
-                    className="text-gray-800 hover:text-blue-600 hover:bg-gray-100 text-lg  font-medium px-4 py-2 inline-flex space-x-1 items-center"
+                    className="text-gray-800 hover:text-blue-600 hover:bg-gray-100 text-sm  font-medium px-4 py-2 inline-flex space-x-1 items-center"
                     onClick={handleShowModal}
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     <span className='mx-2'>
