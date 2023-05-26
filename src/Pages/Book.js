02efd2bcb4c867815374944bd581e7a087e1ff6e
Satyam Katiyar <satyamkatiyar9993@gmail.com>
2023-05-26 12:29:22 +0530

    delete ids

--- src/Pages/Book.js
+++ src/Pages/Book.js
@@ -11,6 +11,7 @@ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const Book = () => {
   const [tab, setTab] = useState("inv");
   const token = useSelector(state => state.token);
+  const selectedInv = useSelector(state => state.selectedInv);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -29,18 +30,49 @@ const Book = () => {
     );
     const InvData = await data.json();
     dispatch(setInv({
-      inv : InvData.invs
+      inv: InvData.invs
     }))
   }
+
+  const deleteSelectedInv = async () => {
+    if (selectedInv.length !== 0) {
+      if(window.confirm('Are you sure you want to delete')){
+
+        const body = {
+          ids : selectedInv
+        };
+        const data = await fetch(`${BACKEND_URL}/api/inv`,
+          {
+            method: 'DELETE',
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(body)
+          }
+        );
+        const InvData = await data.json();
+        if(InvData){
+          fetchInventory();
+        }
+      }
+    }
+  };
+
   return (
     <div>
       <Header />
       <Navbar />
-      <BookTab setTab={setTab} tab={tab}/>
+      <BookTab setTab={setTab} tab={tab} />
       <div className='flex justify-end'>
-      <AddInventory />
+        <div className='mx-2'>
+          <button className='p-2 border border-gray-300 rounded-md text-sm hover:text-blue-500 hover:bg-gray-100' onClick={deleteSelectedInv} >Delete</button>
+        </div>
+        <div className='mx-2 mr-4'>
+          <AddInventory />
+        </div>
       </div>
-      {tab === "inv" && <Inventory /> }
+      {tab === "inv" && <Inventory />}
       {tab === "items" && <>items</>}
       {tab === "exp" && <>exp</>}
     </div>
--- src/components/AddInventory.js
+++ src/components/AddInventory.js
@@ -108,15 +108,12 @@ const AddEmployeeCard = () => {
         <>
             <div className="lg:flex border border-gray-300 cursor-pointer rounded-md">
                 <button
-                    className="text-gray-800 hover:text-blue-600 hover:bg-gray-100 text-sm  font-medium px-4 py-2 inline-flex space-x-1 items-center"
+                    className="text-gray-800 hover:text-blue-600 hover:bg-gray-100 text-sm  font-medium p-2 inline-flex space-x-1 items-center"
                     onClick={handleShowModal}
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                    </svg>
-                    <span className='mx-2'>
-                        Add to Inventory
-                    </span>
+                    </svg>Add to Inventory
                 </button>
             </div>
             {showModal && (
